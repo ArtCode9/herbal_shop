ac7c0b2c214c5495e0d1fe91b5bb3d0922b40d5e
ArtCode9 <alirezatavanmand91@gmail.com>
2025-02-03 01:23:44 -0800

    nav primray change

--- index.php
+++ index.php
@@ -4,18 +4,41 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Herbal Shop</title>
+   <link rel="stylesheet" href="style.css">
+   <link rel="icon" href="src/logo.png">
 </head>
 <body>
-   
+      <div id="container">
+            <div id="navbar">
+                     <div id="navBox1">
+                        <a href="#home">Home</a>
+                        <a href="#news">Products</a>
+                        <a href="#contact">Contact</a>
+                        <a href="#about">About</a>
+                        <div id="iconNavBox">
+                           <img class="iconNav" src="src/shopcart.png" alt="shopcart">
+                           <img class="iconNav" src="src/profile.png" alt="">
+                        </div>
+                     </div>
+                     
+                     <div id="navBox2">
+                           <img src="src/logo.png" alt="herbal shop">
+                     </div>
+                     
+                     <div id="navBox3">
+                           <h1>Herbs Healing Your Pain</h1>
+                     </div>
+            </div>
+      </div>
 
 
 
-<script></script>
+<script src="active.js"></script>
 </body>
 </html>
 <?php  
 
 
-   echo"Hello herbal shop begin";
+   
 
 ?>
