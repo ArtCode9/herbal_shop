a8eb7a014d909a44d8a4e4081013d78704b5d583
ArtCode9 <alirezatavanmand91@gmail.com>
2025-02-04 00:25:00 -0800

    category

--- index.php
+++ index.php
@@ -30,30 +30,163 @@
                            <h1>Herbs Healing Your Pain</h1>
                      </div>
 
-                     <div id="navSlider" class="slider">
-                              <img class="navImg slide" src="src/img3.jpg" alt="image1">      
-                              <img class="navImg slide" src="src/img2.jpg" alt="image1">      
-                              <img class="navImg slide" src="src/shopcart.png" alt="image1">      
-                              <img class="navImg slide" src="src/img6.jpg" alt="image1">      
-                              <img class="navImg slide" src="src/profile.png" alt="image1"> 
-                              
-                              <button class="slider__btn slider__btn--left">&larr;</button>
-                              <button class="slider__btn slider__btn--right">&rarr;</button>
-
-                              
-                     </div>
             </div>
 
+            <div class="category">
+                        <div class="categoryBox">
+                              <img class="categoryImg" src="src/med1.png" alt="med">
+                              <p>تنفسی</p>
+                        </div>
+                        <div class="categoryBox">
+                              <img class="categoryImg" src="src/med2.png" alt="med">
+                              <p>گوارشی</p>
+                        </div>
+                        <div class="categoryBox">
+                              <img class="categoryImg" src="src/med3.png" alt="med">
+                              <p>قلب</p>
+                        </div>
+                        <div class="categoryBox">
+                              <img class="categoryImg" src="src/med4.png" alt="med">
+                              <p>اعصاب</p>
+                        </div>
+                        <div class="categoryBox">
+                              <img class="categoryImg" src="src/med5.png" alt="med">
+                              <p>رژیمی</p>
+                        </div>
+                        <div class="categoryBox">
+                              <img class="categoryImg" src="src/med6.png" alt="med">
+                              <p>کودک</p>
+                        </div>
+            </div>
+
+            <div class="slider">
+                     <img class="cardImgShop" src="src/amino-zink.jpg" alt="amino-zink">   
+                     <img class="cardImgShop" src="src/amino-zink.jpg" alt="amino-zink">   
+                     <img class="cardImgShop" src="src/amino-zink.jpg" alt="amino-zink">   
+                     <img class="cardImgShop" src="src/amino-zink.jpg" alt="amino-zink">   
+                     <img class="cardImgShop" src="src/amino-zink.jpg" alt="amino-zink">   
+                     <img class="cardImgShop" src="src/amino-zink.jpg" alt="amino-zink">   
+                     <img class="cardImgShop" src="src/amino-zink.jpg" alt="amino-zink">   
+            </div>            
+
             <div id="mainSection">
-               <div class="cardShop">1</div>
-               <div class="cardShop">2</div>
-               <div class="cardShop">3</div>
-               <div class="cardShop">4</div>
-               <div class="cardShop">5</div>
-               <div class="cardShop">6</div>
-               <div class="cardShop">7</div>
-               <div class="cardShop">8</div>
-               <div class="cardShop">9</div>
+               <div class="cardShop">
+                        <img class="cardImgShop" src="src/amino-zink.jpg" alt="amino-zink">
+                        <h3>آمینو زینک</h3>
+                        <p>فن آوران زرین گوهر</p>
+                        
+                        <div class="cardShopCart">
+                              <img class="cardPlusShop" src="src/plus.png" alt="">
+                            <div>
+                              <h4>178000</h4>
+                              <p>220000</p>
+                            </div>
+                        </div>
+               </div>
+               <div class="cardShop">
+               <img class="cardImgShop" src="src/amino-zink.jpg" alt="amino-zink">
+                        <h3>آمینو زینک</h3>
+                        <p>فن آوران زرین گوهر</p>
+                        
+                        <div class="cardShopCart">
+                              <img class="cardPlusShop" src="src/plus.png" alt="">
+                            <div>
+                              <h4>178000</h4>
+                              <p>220000</p>
+                            </div>
+                        </div>
+               </div>
+               <div class="cardShop">
+               <img class="cardImgShop" src="src/amino-zink.jpg" alt="amino-zink">
+                        <h3>آمینو زینک</h3>
+                        <p>فن آوران زرین گوهر</p>
+                        
+                        <div class="cardShopCart">
+                              <img class="cardPlusShop" src="src/plus.png" alt="">
+                            <div>
+                              <h4>178000</h4>
+                              <p>220000</p>
+                            </div>
+                        </div>
+               </div>
+               <div class="cardShop">
+               <img class="cardImgShop" src="src/amino-zink.jpg" alt="amino-zink">
+                        <h3>آمینو زینک</h3>
+                        <p>فن آوران زرین گوهر</p>
+                        
+                        <div class="cardShopCart">
+                              <img class="cardPlusShop" src="src/plus.png" alt="">
+                            <div>
+                              <h4>178000</h4>
+                              <p>220000</p>
+                            </div>
+                        </div>
+               </div>
+               <div class="cardShop">
+               <img class="cardImgShop" src="src/amino-zink.jpg" alt="amino-zink">
+                        <h3>آمینو زینک</h3>
+                        <p>فن آوران زرین گوهر</p>
+                        
+                        <div class="cardShopCart">
+                              <img class="cardPlusShop" src="src/plus.png" alt="">
+                            <div>
+                              <h4>178000</h4>
+                              <p>220000</p>
+                            </div>
+                        </div>
+               </div>
+               <div class="cardShop">
+               <img class="cardImgShop" src="src/amino-zink.jpg" alt="amino-zink">
+                        <h3>آمینو زینک</h3>
+                        <p>فن آوران زرین گوهر</p>
+                        
+                        <div class="cardShopCart">
+                              <img class="cardPlusShop" src="src/plus.png" alt="">
+                            <div>
+                              <h4>178000</h4>
+                              <p>220000</p>
+                            </div>
+                        </div>
+               </div>
+               <div class="cardShop">
+               <img class="cardImgShop" src="src/amino-zink.jpg" alt="amino-zink">
+                        <h3>آمینو زینک</h3>
+                        <p>فن آوران زرین گوهر</p>
+                        
+                        <div class="cardShopCart">
+                              <img class="cardPlusShop" src="src/plus.png" alt="">
+                            <div>
+                              <h4>178000</h4>
+                              <p>220000</p>
+                            </div>
+                        </div>
+               </div>
+               <div class="cardShop">
+               <img class="cardImgShop" src="src/amino-zink.jpg" alt="amino-zink">
+                        <h3>آمینو زینک</h3>
+                        <p>فن آوران زرین گوهر</p>
+                        
+                        <div class="cardShopCart">
+                              <img class="cardPlusShop" src="src/plus.png" alt="">
+                            <div>
+                              <h4>178000</h4>
+                              <p>220000</p>
+                            </div>
+                        </div>
+               </div>
+               <div class="cardShop">
+               <img class="cardImgShop" src="src/amino-zink.jpg" alt="amino-zink">
+                        <h3>آمینو زینک</h3>
+                        <p>فن آوران زرین گوهر</p>
+                        
+                        <div class="cardShopCart">
+                              <img class="cardPlusShop" src="src/plus.png" alt="">
+                            <div>
+                              <h4>178000</h4>
+                              <p>220000</p>
+                            </div>
+                        </div>
+               </div>
             </div>
 
             <div class="footer">
@@ -84,6 +217,12 @@
             </div>
       </div>
 
+ <!--  <?php foreach($objects as $object):?>
+                  <div class="product">
+                        <div><?= $object->title ?></div>
+                        <div><?= $object->description ?></div>
+                  </div>
+       <?php endforeach;?> -->
 
 
 <script src="active.js"></script>
